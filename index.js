22357e6a7087760c5e6ab25759e5304a01d6da25
kiyoung <popping2606@gmail.com>
2020-10-24 09:16:55 +0900

    chat is done.

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 var app = require('express')()
 var http = require('http').createServer(app)
+var io = require('socket.io')(http)
 
+//open file
+app.set('trust proxy', '8.8.8.8')
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/index.html')
 })
 
-const port = process.env.PORT || 4000
+//make io
+io.on('connection', (socket) => {
+  let address = socket.handshake.address;
+  address = address === '::1' ? 'admin' : 'guest'+address.split(':')[3].substring(7)
+	socket.on('chat message', (msg) => {
+		io.emit('chat message', {address, msg})
+	})
+})
 
-app.listen(port, () => {
+//get IP
+const getIp = (req) => req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress
+
+//open Server
+const port = process.env.PORT || 4000
+http.listen(port, () => {
 	console.log(`Listening on port: ${port}\nConnect to http://localhost:${port}`)
 })

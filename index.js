eb2ca06902c761a00aff6e3136bb3bb7c60e33e2
kiyoung <popping2606@gmail.com>
2020-10-27 20:51:49 +0900

    making board

--- index.js
+++ index.js
@@ -68,10 +68,10 @@ io.on('connection', (socket) => {
 
 	//접속한 사람에게 저장된 메시지를 방출(emit), 인사문구
 	io.to(socket.id).emit('get msgs', msgs)
-	io.to(socket.id).emit('new connect', `${address}님 환영합니다 :D`)
+	io.to(socket.id).emit('new connect', {client: address, isMe: true})
 
 	//나를 제외한 전체에게 접속한 사람 address를 방출
-	socket.broadcast.emit('new connect', `${address}님이 접속하셨습니다.`)
+	socket.broadcast.emit('new connect', {client: address, isMe: false})
 
 	//메시지 입력 받으면 메시지를 msgs에 저장하고 입력 받은 메시지를 방출(emit)
 	socket.on('chat message', (msg) => {
@@ -81,7 +81,7 @@ io.on('connection', (socket) => {
 		}
 
 		let time = date.getFullYear()
-		time += date.getMonth() < 10 ? '/0' + (date.getMonth()+1) : '/' + (date.getMonth()+1)
+		time += (date.getMonth()+1) < 10 ? '/0' + (date.getMonth()+1) : '/' + (date.getMonth()+1)
 		time += date.getDate() < 10 ? '/0' + date.getDate() : '/' + date.getDate()
 		time += date.getHours() < 10 ? ' 0' + date.getHours() : ' ' + date.getHours()
 		time += date.getMinutes() < 10 ? ':0' + date.getMinutes() : ':' + date.getMinutes()

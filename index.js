b74c43bbc09400b40682522bf33bc65eee3b5d43
kiyoung <popping2606@gmail.com>
2020-10-30 14:30:26 +0900

    add blackLists, remove preventing recents

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const io = require('socket.io')(server, {
 })
 const cors = require('cors')
 
+const buttons = ['yellow', true, true, true, true, true, true, true, true, true, true]
+const allClients = []
+const clients = []
+let blackLists = ['바보야']
+let msgs = []
+let recents = []
+let weeks = []
+let months = []
+
+
 const origins = ['http://localhost:4000', 'http://127.0.0.1:5500', 'http://192.168.0.4:5500', 'https://imki123.github.io', 'https://socket-imki123.herokuapp.com/']
 let corsOptions
 const corsOptionsDelegate = function (req, callback) {
@@ -41,15 +51,6 @@ app.post('/setCookie', (req, res) => {
 })
 
 //make io
-
-let msgs = []
-const allClients = []
-const clients = []
-const buttons = ['yellow', true, true, true, true, true, true, true, true, true, true]
-let recents = []
-let weeks = []
-let months = []
-
 //io에 접속하면 현재 상태 전파
 io.on('connection', (socket) => {
 	//접속 시 address 만들고 접속자 수 증가
@@ -82,45 +83,48 @@ io.on('connection', (socket) => {
 			}
 			//버튼이 모두 같으면 색 변경
 			let winner
-			//색 바꾸기 성공
-			if (changeColor) {
+			//블랙리스트가 아니면 배경색 바꾸기
+			if (blackLists.indexOf(address) === -1 && changeColor) {
 				if (buttons[0] === 'yellow') buttons[0] = 'gray'
 				else buttons[0] = 'yellow'
 				//recents(최근 성공한 사람) 추가하고 emit하기
 				client = allClients[findClient(allClients, socket)].address
-				//최근 성공한 사람이 아니면 최근에 추가.
-				if (recents.length === 0 || recents[recents.length - 1].client !== client) {
-					winner = true
-					let recent = { client: client, date: new Date() }
-					recents.push(recent)
-					if (recents.length > 5) recents.shift() //최근은 5개까지만
-					//weeks에 추가하는데 이미 있으면 count ++
-					let weekIdx, monthIdx
-					for (let i = 0; i < 5; i++) {
-						//weeks 카운트 증가
-						if (weeks[i] && weeks[i].client === client) {
-							weeks[i].count++
-							weekIdx = i
-						}
-						//months 카운트 증가
-						if (months[i] && months[i].client === client) {
-							months[i].count++
-							monthIdx = i
-						}
+				winner = true
+				let recent = { client: client, date: new Date() }
+				recents.push(recent)
+				if (recents.length > 5) recents.shift() //최근은 5개까지만
+				//weeks에 추가하는데 이미 있으면 count ++
+				let weekIdx, monthIdx
+				for (let i = 0; i < weeks.length; i++) {
+					//weeks 카운트 증가
+					if (weeks[i] && weeks[i].client === client) {
+						weeks[i].count++
+						weekIdx = i
 					}
-					//weeks에 없으면 추가하고 count 1
-					if (weekIdx === undefined) {
-						weeks.push({ ...recent, count: 1 })
+				}
+				for (let i = 0; i < months.length; i++) {
+					//months 카운트 증가
+					if (months[i] && months[i].client === client) {
+						months[i].count++
+						monthIdx = i
 					}
-					if (monthIdx === undefined) {
-						months.push({ ...recent, count: 1 })
-					}
-					weeks.sort((a, b) => b.count - a.count)
-					months.sort((a, b) => b.count - a.count)
+				}
+				//weeks에 없으면 추가하고 count 1
+				if (weekIdx === undefined) {
+					weeks.push({ ...recent, count: 1 })
+				}
+				if (monthIdx === undefined) {
+					months.push({ ...recent, count: 1 })
+				}
+				weeks.sort((a, b) => b.count - a.count)
+				months.sort((a, b) => b.count - a.count)
+				/* //최근 성공한 사람이 아니면 최근에 추가.
+				 if (recents.length === 0 || recents[recents.length - 1].client !== client) {
 				} else if (recents.length > 0 && recents[recents.length - 1].client === client) {
 					winner = false
-				}
+				} */
 			}
+			
 			io.to(socket.id).emit('buttons', { buttons, recents, weeks, months, winner })
 			//나를 제외한 전체에게 접속한 사람 address를 방출
 			socket.broadcast.emit('buttons', { buttons, recents, weeks, months })
@@ -161,6 +165,9 @@ io.on('connection', (socket) => {
 
 	//메시지 입력 받으면 메시지를 msgs에 저장하고 입력 받은 메시지를 방출(emit)
 	socket.on('chat message', (msg) => {
+		if(blackLists.indexOf(address) > -1){ //닉네임이 블랙리스트에 있으면 채팅 불가
+			return
+		}
 		let date = new Date()
 		//서버에 올리면 한국시간으로 적용.
 		if (process.env.NODE_ENV === 'production') {
@@ -175,22 +182,25 @@ io.on('connection', (socket) => {
 		msgs.push({ address, msg, time }) //msgs에 메시지들 저장
 
 		//대화삭제 트리거
-		if (msg === '대화삭제') {
-			msgs = []
-			io.emit('clearMsgs')
+		if (msg.split(' ')[0] === '대화삭제') {
+			msgs = msgs.filter(i=> i.address !== msg.split(' ')[1])
+			io.emit('clearMsgs') //채팅창 지우고
+			setTimeout(function(){
+				io.to(socket.id).emit('get msgs', msgs) //새로 변경된 메시지 방출
+			})
+			blackLists.push(msg.split(' ')[1]) //블랙리스트에 추가
 		}else if (msg.split(' ')[0] === '랭킹삭제') {
 			//랭킹삭제 트리거
 			let rank = Number(msg.split(' ')[1]) - 1
-			let client = weeks[rank].client
+			let client = months[rank].client
 			recents = recents.filter(i=>i.client !== client)
 			weeks = weeks.filter(i=>i.client !== client)
 			months = months.filter(i=>i.client !== client)
 			io.emit('buttons', { buttons, recents, weeks, months })
+			blackLists.push(client) //블랙리스트에 추가
 		}else {
 			io.emit('chat message', { address, msg, time })
 		}
-
-		
 	})
 })
 
